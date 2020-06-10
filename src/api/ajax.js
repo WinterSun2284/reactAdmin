99ae37f0ccedbf6428c51f5a0770e5f5f0aaec2a
wintersun <wintersun2284@163.com>
2020-06-10 11:44:35 +0800

    2020\06\010update

---
+++ src/api/ajax.js
@@ -0,0 +1,12 @@
+/*
+能发送异步请求的函数模块
+ */
+import axios from "axios"
+
+export default function ajax(url, data = {}, type = 'GET') {
+    if (type === 'GET') {
+        return axios.get(url, {params: data})
+    } else {
+        return axios.post(url,data)
+    }
+}
