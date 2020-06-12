f22eeac1367838c065964cec97bdc98c4235c863
wintersun <wintersun2284@163.com>
2020-06-12 15:55:01 +0800

    2020\06\12-update

--- src/components/left-nav/index.jsx
+++ src/components/left-nav/index.jsx
@@ -26,7 +26,7 @@ class LeftNav extends Component {
     render() {
         return (
             <div className="left-nav">
-                <Link to='/' className="left-nav-header">
+                <Link to='/home' className="left-nav-header">
                     <img src={logo} alt="logo"/>
                     <h1>wintersun</h1>
                 </Link>
@@ -36,7 +36,7 @@ class LeftNav extends Component {
                     mode="inline"
                     theme="dark"
                 >
-                  {/* <Menu.Item key="/home" >
+                   <Menu.Item key="/home" >
                        <HomeOutlined/>
                         <Link to="/home">首页</Link>
                     </Menu.Item>
@@ -50,66 +50,12 @@ class LeftNav extends Component {
                         <Menu.Item key="/charts/bar" icon={<BarChartOutlined />}><Link to="/charts/bar">柱状图</Link> </Menu.Item>
                         <Menu.Item key="/charts/line" icon={<LineChartOutlined />}><Link to='/charts/line'>折线图</Link></Menu.Item>
                         <Menu.Item key="/charts/pie" icon={<PieChartOutlined />}><Link to='/charts/pie'>饼图</Link></Menu.Item>
-                    </SubMenu>*/}
+                    </SubMenu>
 
-                    {/*{*/}
-                    {/*    this.getMenuNodes(menuList)*/}
-                    {/*}*/}
                 </Menu>
             </div>
 
         );
-    }
-
-    /**
-     * 根据menu的数据数组生成对应的标签数组
-     * @param menuList
-     */
-    getMenuNodes(menuList) {
-
-        return menuList.render((pre,item)=>{
-            if (!item.children) {
-                pre.push(
-                    <Menu.Item key={item.key}>
-                        {item.icon}
-                        <Link to={item.key}>{item.title}</Link>
-                    </Menu.Item>
-                )
-            } else {
-                pre.push(
-                    <SubMenu key="sub1" icon={<AppstoreAddOutlined/>} title="商品">
-                        <Menu.Item key="/category" icon={<MenuOutlined/>}><Link to="/category">品类管理</Link> </Menu.Item>
-                        <Menu.Item key="/product" icon={<ToolOutlined/>}><Link to='/product'>商品管理</Link></Menu.Item>
-                    </SubMenu>
-                )
-            }
-        })
-
-        /*return menuList.map(item => {
-            // {
-            //     title: '首页', // 菜单标题名称
-            //         key: '/home', // 对应的path
-            //     icon: 'home', // 图标名称
-            //     isPublic: true, // 公开的
-            // },
-            /!* <Menu.Item key="/home" icon={<HomeOutlined/>}>
-                 <Link to="/home">首页</Link>
-             </Menu.Item>*!/
-            // if (!item.children) {
-            //     return (
-            //         <Menu.Item key={item.key}>
-            //             <Link to={item.key}>{item.title}</Link>
-            //         </Menu.Item>
-            //     )
-            // } else {
-            //     return (
-            //         <SubMenu key="sub1" icon={<AppstoreAddOutlined/>} title="商品">
-            //             <Menu.Item key="/category" icon={<MenuOutlined/>}><Link to="/category">品类管理</Link> </Menu.Item>
-            //             <Menu.Item key="/product" icon={<ToolOutlined/>}><Link to='/product'>商品管理</Link></Menu.Item>
-            //         </SubMenu>
-            //     )
-            // }
-        })*/
     }
 }
 
--- src/pages/admin/admin.jsx
+++ src/pages/admin/admin.jsx
@@ -1,11 +1,19 @@
 import React, {Component} from 'react';
-import {Redirect} from "react-router-dom";
+import {Redirect,Route,Switch} from "react-router-dom";
 import {Layout} from "antd";
 
 
 import memoryUtils from "../../utils/memoryUtils";
 import LeftNav from "../../components/left-nav";
 import Header from "../../components/header";
+import Home from "../home/home";
+import Category from "../category/category";
+import Product from "../product/product";
+import Role from "../role/role";
+import User from "../user/user";
+import Bar from "../charts/Bar";
+import Line from "../charts/line";
+import Pie from "../charts/pie";
 
 const {Footer, Sider, Content} = Layout
 
@@ -27,7 +35,19 @@ class Admin extends Component {
                 <Layout>
                     <Header>
                     </Header>
-                    <Content style={{backgroundColor:"#fff"}}>Content</Content>
+                    <Content style={{backgroundColor:"#fff"}}>
+                        <Switch>
+                            <Redirect from='/' exact to='/home'/>
+                            <Route path='/home' component={Home}/>
+                            <Route path='/category' component={Category}/>
+                            <Route path='/product' component={Product}/>
+                            <Route path='/user' component={User}/>
+                            <Route path='/role' component={Role}/>
+                            <Route path="/charts/bar" component={Bar}/>
+                            <Route path="/charts/pie" component={Pie}/>
+                            <Route path="/charts/line" component={Line}/>
+                        </Switch>
+                    </Content>
                     <Footer style={{textAlign:"center",color:"#ccc"}}>推荐使用谷歌浏览器，可以获得更佳页面操作体验</Footer>
                 </Layout>
             </Layout>
